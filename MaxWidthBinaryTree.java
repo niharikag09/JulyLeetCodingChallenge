48327468fdc1d268fb3ef941586a29dac2b2f371
Niharika Gupta <niharikag09@gmail.com>
2020-07-10 15:53:31 -0700

    Week2- Day2 July 09,2020

---
+++ MaxWidthBinaryTree.java
@@ -0,0 +1,94 @@
+/*
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+Example 4:
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+Note: Answer will in the range of 32-bit signed integer.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max=0;
+    HashMap<Integer,Integer> leftmostAtDepth= new HashMap<>();
+    
+    public int widthOfBinaryTree(TreeNode root) {
+
+        findWidth(root,0,0);
+        return max;
+    }
+    
+    private void findWidth(TreeNode root, int depth, int position){
+        if(root==null) return;
+        
+        if(!leftmostAtDepth.containsKey(depth)) leftmostAtDepth.put(depth,position);
+        
+        max=Math.max(max, position-leftmostAtDepth.get(depth)+1);
+        
+        findWidth(root.left,depth+1,2*position);
+        findWidth(root.right,depth+1,2*position+1);
+    }
+}
